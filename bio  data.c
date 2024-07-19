cf291dab9cbb9d8a947d143e50b08cd0cd7309a9
Himanshu vaishnav9932 <vaishnavhimanshu772@gmail.com>
2024-07-19 18:43:23 +0530

    Add files via upload

---
+++ bio  data.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   printf("name:vaishnav himanshu\n");
+   printf("school:rmg maheswari english high school\n");
+   printf("age:18");
+
+
+
+
+
+}
