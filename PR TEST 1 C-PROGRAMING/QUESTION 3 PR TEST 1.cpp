21d9933d5808f81c3e8ad30ae0d510e57a3f7d99
Himanshu vaishnav9932 <vaishnavhimanshu772@gmail.com>
2024-09-21 15:00:06 +0530

    Add files via upload

---
+++ PR TEST 1 C-PROGRAMING/QUESTION 3 PR TEST 1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("enter A = ");
+	scanf("%d",&a); 
+	
+	printf("enter B = ");
+	scanf("%d",&b); 
+	
+	c=a+b;
+	
+    printf("Third angle: %d",c-180);
+}
